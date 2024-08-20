303cd4575b6204ff796a5e367700bfe3a3d31275
koster18 <89853641+koster18@users.noreply.github.com>
2024-08-20 08:52:42 +0400

    Add files via upload
    0001 Pattern via Spring Initializer
    
    Pattern for project created with Spring Initializer https://start.spring.io

---
+++ src/test/java/ru/sterkhovkv/SensorDB/SensorDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sterkhovkv.SensorDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
